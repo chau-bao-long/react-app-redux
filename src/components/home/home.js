99f91ac36568c499e9e1cc2862a428d15f9ea199
Tran Nhu Long <nhulongctk10@gmail.com>
2017-05-04 08:48:38 +0700

    add home component

---
+++ src/components/home/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = React.createClass({
+  render: function() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+          <h1>Theme example</h1>
+          <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Home;
+
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import Login from './components/login/Login';
 import ShowBook from './components/showBook';
+import Home from './components/home/home';
 import App from './components/app';
 import reducers from './reducers';
 import DevTools from './containers/DevTools';
@@ -16,6 +17,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers, enhancer)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Home} />
         <Route path="login" component={Login}></Route>
         <Route path="books" component={ShowBook}></Route>
       </Route>
